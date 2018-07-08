da89e89e4b867b671d5e42d329a5245b8968a9df
Sean Grady <vedicmonk@gmail.com>
2018-07-07 22:24:05 -0700

    Added calls to find default sample rate so I don't have to look it up and set it manually every time

--- sounddevice_testing.py
+++ sounddevice_testing.py
@@ -6,7 +6,13 @@ import numpy as np
 import sounddevice as sd
 from collections import deque
 
+
 input_deque = deque()
+default_microphone = sd.default.devices[0]
+microphone_sample_rate = sd.query_devices(
+    default_microphone
+)['default_samplerate']
+
 
 def microphone_callback(in_data, frames, time, status):
     deque.append(in_data)
