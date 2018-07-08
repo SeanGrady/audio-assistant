5cb4f087e75ef000234a95117262e7e8ca20ba36
Sean Grady <vedicmonk@gmail.com>
2018-07-08 10:23:32 -0700

    bug fixes

--- sounddevice_testing.py
+++ sounddevice_testing.py
@@ -4,11 +4,14 @@ import numpy as np
 import shutil
 import numpy as np
 import sounddevice as sd
+import matplotlib as mpl
+mpl.use('tkagg')
+from matplotlib import pyplot as plt
 from collections import deque
 
 
 input_deque = deque()
-default_microphone = sd.default.devices[0]
+default_microphone = sd.default.device[0]
 microphone_sample_rate = sd.query_devices(
     default_microphone
 )['default_samplerate']
@@ -19,16 +22,19 @@ def microphone_callback(in_data, frames, time, status):
 
 
 def set_defaults():
-    sd.defaults.channels = [1,2]
+    sd.default.channels = [1,2]
 
 
 if __name__ == '__main__':
-    audio_data = np.ndarry()
     set_defaults()
 
-    seconds_to_record = 5
+    seconds_to_record = .5
 
     recording = sd.rec(int(seconds_to_record * microphone_sample_rate))
+    sd.wait()
+    plt.plot(recording)
+    plt.show()
+    import pdb;pdb.set_trace
 
 
 #    with sd.InputStream(channels=2, callback=microphone_callback):
