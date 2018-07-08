86b536f6291597b43d5f6a4af45c6c40731c196b
Sean Grady <vedicmonk@gmail.com>
2018-07-07 22:26:02 -0700

    Added a function to set the required defaults for sd.rec(), sd.Stream(), etc

--- sounddevice_testing.py
+++ sounddevice_testing.py
@@ -18,9 +18,17 @@ def microphone_callback(in_data, frames, time, status):
     deque.append(in_data)
 
 
+def set_defaults():
+    sd.defaults.channels = [1,2]
+
 
 if __name__ == '__main__':
     audio_data = np.ndarry()
+    set_defaults()
+
+    seconds_to_record = 5
+
+    recording = sd.rec(int(seconds_to_record * microphone_sample_rate))
 
 
 #    with sd.InputStream(channels=2, callback=microphone_callback):
